fff24add790bda643df981443648a25da484e198
Daniel <gildenbrand.daniel@gmail.com>
2025-03-01 14:56:43 -0500

    in next head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono, Lobster } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,12 +30,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-3998650725257627" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased animate-gradient bg-gradient-to-br from-rose-300 from-[27%] via-sky-500 via-[60%] to-indigo-500 to-[90%]`}
       >
         {children}
       </body>
-      <meta name="google-adsense-account" content="ca-pub-3998650725257627" />
     </html>
   );
 }
